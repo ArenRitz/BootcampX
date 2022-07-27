7d302e126220feb2f24582bf65365fb66902247a
ArenRitz <aren.dolbakian@gmail.com>
2022-07-27 17:51:52 -0400

    Create queries for bootcampx app

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,28 @@
+let cohortInput = process.argv[2];
+
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+
+pool.query(`
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort, count(assistance_requests.*) AS total_assistances
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${cohortInput}%'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teachers.name;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.teacher} assisted in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
